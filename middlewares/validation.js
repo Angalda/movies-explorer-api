a0da521f115c6ad49994bd3ca74276b6fac25cc9
Angalda <angalda@mail.ru>
2022-08-12 16:43:13 +0300

    feat: config

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -12,31 +12,23 @@ const validateUrl = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w.-]*)*\/?$/;
 
 module.exports.validationCreateUser = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(validateUrl),
+    name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
 module.exports.validationLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ tlds: { allow: false } }),
-    password: Joi.string().required().min(8),
+    password: Joi.string().required(),
   }),
 });
 
 module.exports.validationUpdateUser = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-  }),
-});
-
-module.exports.validationGetUserId = celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().hex().length(24).required(),
+    email: Joi.string().min(2).max(30),
   }),
 });
 
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const { validationUpdateUser } = require('../middlewares/validation');
 
 const {
   updateUser, getUser,
 } = require('../controllers/users');
 
 router.get('/me', getUser);
-router.patch('/me', updateUser);
+router.patch('/me', validationUpdateUser, updateUser);
 
 module.exports = router;
